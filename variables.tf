938698cce166bc129423faec32e46fa295bdf61f
Henrik Knutsen <46495473+hknutsen@users.noreply.github.com>
2024-08-26 18:10:08 +0200

    chore: update variable descriptions and add features list

--- variables.tf
+++ variables.tf
@@ -1,40 +1,51 @@
 variable "workspace_name" {
   description = "The name of this Databricks workspace."
   type        = string
+  nullable    = false
 }
 
 variable "resource_group_name" {
   description = "The name of the resource group to create the resources in."
   type        = string
+  nullable    = false
 }
 
 variable "location" {
   description = "The location to create the resources in."
   type        = string
+  nullable    = false
 }
 
 variable "sku" {
-  description = "The SKU of this Databricks workspace."
+  description = "The SKU of this Databricks workspace. Value must be \"standard\" or \"premium\"."
   type        = string
   default     = "standard"
+
+  validation {
+    condition     = contains(["standard", "premium"], var.sku)
+    error_message = "SKU must be \"standard\" or \"premium\"."
+  }
 }
 
 variable "managed_resource_group_name" {
   description = "The name of the resource group to create the managed Databricks resources in."
   type        = string
   default     = null
+  nullable    = true
 }
 
 variable "log_analytics_workspace_id" {
-  description = "The ID of the Log Analytics workspace to send diagnostics to."
+  description = "The ID of the Log Analytics workspace to send diagnostics to. Required if value of sku is \"premium\"."
   type        = string
   default     = null
+  nullable    = true
 }
 
 variable "diagnostic_setting_name" {
   description = "The name of this diagnostic setting."
   type        = string
   default     = "audit-logs"
+  nullable    = false
 }
 
 # Enable all log categories that do not cost to export
@@ -55,10 +66,13 @@ variable "diagnostic_setting_enabled_log_categories" {
     "ssh",
     "workspace"
   ]
+
+  nullable = false
 }
 
 variable "tags" {
   description = "A map of tags to assign to the resources."
   type        = map(string)
   default     = {}
+  nullable    = false
 }
