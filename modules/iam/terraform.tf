bfc50e524c95553b698bc5ac42587be9125a627b
Henrik Knutsen <46495473+hknutsen@users.noreply.github.com>
2026-01-23 13:07:12 +0100

    fix

--- modules/iam/terraform.tf
+++ modules/iam/terraform.tf
@@ -3,7 +3,7 @@ terraform {
   required_version = ">= 1.3.0"
 
   required_providers {
-    azurerm = {
+    databricks = {
       source  = "databricks/databricks"
       # Databricks provider version 1.31.0 required to use the "databricks_current_config" data source
       version = ">= 1.31.0"
