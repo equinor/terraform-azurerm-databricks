f3c2ee84a566b4f3df91d8a19c3f706984b2fb3c
Henrik Knutsen <46495473+hknutsen@users.noreply.github.com>
2026-01-26 09:35:30 +0100

    docs

--- modules/iam/variables.tf
+++ modules/iam/variables.tf
@@ -1,7 +1,7 @@
 variable "external_groups" {
-  # TODO: add a description
+  description = "A map of external groups to assign to the Databricks workspace. To assign a user, group or service principal from Microsoft Entra ID, the \"external_id\" should match the Microsoft Entra ID \"objectId\"."
   type = map(object({
-    external_id           = string # Object ID from Entra ID
+    external_id           = string
     admin_access          = optional(bool, false)
     workspace_access      = optional(bool, true)
     databricks_sql_access = optional(bool, true)
