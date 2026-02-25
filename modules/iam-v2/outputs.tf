00cebba295485cf46a45659a9640b98d73ff7349
Henrik Knutsen <46495473+hknutsen@users.noreply.github.com>
2026-02-25 09:20:38 +0100

    refactor

--- modules/iam-v2/outputs.tf
+++ modules/iam-v2/outputs.tf
@@ -1,9 +1,9 @@
-output "external_group_display_names" {
-  description = "A map of display names for the external groups that were assigned to the workspace."
-  value       = { for k, v in data.databricks_group.external : k => v.display_name }
+output "group_display_names" {
+  description = "A map of display names for the external groups that were assigned to the Databricks workspace."
+  value       = { for k, v in data.databricks_group.this : k => v.display_name }
 }
 
-output "external_service_principal_application_ids" {
-  description = "A map of application IDs for the external service principals that were assigned to the workspace."
-  value       = { for k, v in data.databricks_service_principal.external : k => v.application_id }
+output "service_principal_application_ids" {
+  description = "A map of application IDs for the external service principals that were assigned to the Databricks workspace."
+  value       = { for k, v in data.databricks_service_principal.this : k => v.application_id }
 }
