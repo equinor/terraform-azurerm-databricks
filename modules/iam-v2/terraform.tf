b753d5498fb3b7eac777356958544ce827cba237
Henrik Knutsen <HKN@equinor.com>
2026-09-10 10:10:50 +0200

    refactor(iam-v2): use official data sources

--- modules/iam-v2/terraform.tf
+++ modules/iam-v2/terraform.tf
@@ -5,19 +5,8 @@ terraform {
   required_providers {
     databricks = {
       source = "databricks/databricks"
-      # Databricks provider version 1.31.0 required to use the "databricks_current_config" data source
-      version = ">= 1.31.0"
-    }
-
-    time = {
-      source = "hashicorp/time"
-      # Time provider version 0.5.0 required to use the "time_sleep" resource
-      version = ">= 0.5.0"
-    }
-
-    external = {
-      source  = "hashicorp/external"
-      version = ">= 2.0.0"
+      # Databricks provider version 1.131.0 required to use the "databricks_workspace_iam_external_group_v2" and "databricks_workspace_iam_external_service_principal_v2" resources
+      version = ">= 1.131.0"
     }
   }
 }
